7ab242f3a4c17543028d213ef823ed8d6d031154
ivantayyy <69101445+ivantayyy@users.noreply.github.com>
2021-06-14 22:42:36 +0800

    loginscreen edits

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,21 +1,102 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-
-export default function LoginScreen({ navigation }) {
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
+import Validation from "../components/Validation";
+export default function LoginScreen({ navigation, Component }) {
   return (
-    <View>
-      <Text> login</Text>
-      {/*button to navigate to registerscreen*/}
-      <TouchableOpacity onPress={() => navigation.push("RegisterScreen")}>
-        <Text>register</Text>
+    <View style={styles.Container}>
+      <Text style={styles.welcomeword}>
+        {/*<strong>Welcome to HealthFirst</strong>*/}
+        Welcome to HealthFirst
+      </Text>
+
+      {/*<Image style={styles.image} source={require("../images/logo.jpg")} />*/}
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#bababa"
+        autoCapitalize="none"
+        //onChangeText={this.handleEmail}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#bababa"
+        autoCapitalize="none"
+        //onChangeText={this.handlePassword}
+      />
+
+      <TouchableOpacity
+        style={styles.submitButton}
+        onPress={() => this.login(this.state.email, this.state.password)}
+      >
+        <Text style={styles.submitButtonText}> Submit </Text>
       </TouchableOpacity>
-      {/*button to navigate to homescreen*/}
+      <Text style={styles.registration}> "Don't have a account? Register"</Text>
+      <TouchableOpacity
+        style={styles.registerpush}
+        onPress={() => navigation.push("RegisterScreen")}
+      >
+        <Text>here</Text>
+      </TouchableOpacity>
+
       <TouchableOpacity
         onPress={() => navigation.navigate("Home", { screen: "HomeScreen" })}
       >
-        <Text>home</Text>
+        <Text>To Home Page Button</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  welcomeword: {
+    fontSize: 24,
+    color: "blue",
+    justifyContent: "center",
+    paddingLeft: 30,
+    marginTop: 50,
+  },
+  image: {
+    marginBottom: 100,
+    marginLeft: 60,
+    height: 50,
+    width: 200,
+  },
+  registration: {
+    alignContent: "center",
+    marginLeft: 60,
+  },
+  registerpush: {
+    fontSize: 16,
+    fontFamily: "calibri",
+    marginLeft: 150,
+    textDecorationLine: "underline",
+  },
+  input: {
+    margin: 15,
+    height: 40,
+    borderColor: "#000000",
+    borderWidth: 1,
+    paddingLeft: 125,
+  },
+  submitButton: {
+    backgroundColor: "#000000",
+    padding: 10,
+    margin: 15,
+    marginLeft: 125,
+    marginRight: 130,
+    height: 40,
+  },
+  submitButtonText: {
+    color: "white",
+  },
+});
--- components/Validation.js
+++ components/Validation.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 class Validation extends Component {
-   state = {
-      email: '',
-      password: ''
-   }
-   handleEmail = (text) => {
-      this.setState({ email: text })
-   }
-   handlePassword = (text) => {
-      this.setState({ password: text })
-   }
-   login = (email, pass) => {
-      alert('email: ' + email + ' password: ' + pass)
-   }
+  state = {
+    email: "",
+    password: "",
+  };
+  //handleEmail = (text) => {
+  //this.setState({ email: text });
+  //};
+  //handlePassword = (text) => {
+  //this.setState({ password: text });
+  //};
+  //login = (email, pass) => {
+  //alert("email: " + email + " password: " + pass);
+  //};
 
-   // can send this information for validating the user 
+  // can send this information for validating the user
 
-
-   render() {
-      return (
-       <Text>  </Text>   
-      )
-   }
+  render() {
+    return <Text> </Text>;
+  }
 }
-export default Validation
+export default Validation;
