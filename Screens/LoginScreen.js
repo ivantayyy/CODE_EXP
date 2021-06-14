b6c426e8fd3b8ab2bdd03acd96e90365ece58ee0
hackedbyskittles <hackedbyskittles@gmail.com>
2021-06-14 17:21:38 +0800

    Login Screen done
    it works when its on the web

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,21 +1,95 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from "react-native";
+import Validation from "../components/Validation"
 
-export default function LoginScreen({ navigation }) {
+   
+
+export default function LoginScreen({ navigation, Component }) {
   return (
-    <View>
-      <Text> login</Text>
-      {/*button to navigate to registerscreen*/}
-      <TouchableOpacity onPress={() => navigation.push("RegisterScreen")}>
-        {"register"}
+
+    <View style = {styles.Container}>
+      <Text style={styles.welcomeword}><strong>Welcome to HealthFirst</strong></Text>
+
+    <Validation />
+    
+
+      <Image style={styles.image}
+            source={require(
+                '../images/logo.jpg'
+      )}
+          />
+            <TextInput style = {styles.input}
+               placeholder = "Email"
+               placeholderTextColor = "#bababa"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
+            
+            <TextInput style = {styles.input}
+               placeholder = "Password"
+               placeholderTextColor = "#bababa"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+            
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.login(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Submit </Text>
+            </TouchableOpacity>
+            <Text style={styles.registration}> "Don't have a account? Register"</Text>
+            <TouchableOpacity style={styles.registerpush} onPress={() => navigation.push("RegisterScreen")}>
+        {"here"}
       </TouchableOpacity>
-      {/*button to navigate to homescreen*/}
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Home", { screen: "HomeScreen" })}
-      >
-        {"home"}
-      </TouchableOpacity>
+
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  welcomeword: {
+    fontSize:24,
+    color: 'blue',
+    justifyContent: 'center',
+    paddingLeft: 30,
+    marginTop:50
+  },
+     image: {
+     marginBottom:100,
+     marginLeft:60,
+    height: 50,
+    width: 200,
+   },
+   registration:{
+     alignContent: 'center',
+     marginLeft: 60
+   },
+   registerpush: {
+     fontSize:16,
+     fontFamily:'calibri',
+     marginLeft:150,
+     textDecorationLine: 'underline'
+     
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft:125
+   },
+   submitButton: {
+      backgroundColor: '#000000',
+      padding: 10,
+      margin: 15,
+      marginLeft: 125,
+      marginRight:130,
+      height: 40,
+      
+   },
+   submitButtonText:{
+      color: 'white'
+   }
+})
---
+++ components/Validation.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
+
+class Validation extends Component {
+   state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (text) => {
+      this.setState({ password: text })
+   }
+   login = (email, pass) => {
+      alert('email: ' + email + ' password: ' + pass)
+   }
+
+   // can send this information for validating the user 
+
+
+   render() {
+      return (
+       <Text>  </Text>   
+      )
+   }
+}
+export default Validation
